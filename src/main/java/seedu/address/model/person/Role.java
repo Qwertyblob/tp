8104e29b9613816c8e26e60e61197b9f17a0ddb6
jarvis <jarvischua8002@gmail.com>
2025-10-08 21:23:11 +0800

    Add Role class to represent the role of a Person

---
+++ src/main/java/seedu/address/model/person/Role.java
@@ -0,0 +1,60 @@
+package seedu.address.model.person;
+
+import seedu.address.model.tag.Tag;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class Role {
+
+    public static final String MESSAGE_CONSTRAINTS = "Roles are only either \"tutor\" or \"student\".";
+    public static final String VALIDATION_REGEX = "(^student|tutor}$)";
+
+    public final String role;
+
+    /**
+     * Constructs a {@code Role}.
+     *
+     * @param role A valid role.
+     */
+    public Role(String role) {
+        requireNonNull(role);
+        checkArgument(isValidRole(role), MESSAGE_CONSTRAINTS);
+        this.role = role;
+    }
+
+    /**
+     * Returns true if a given string is a valid role.
+     */
+    public static boolean isValidRole(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Tag)) {
+            return false;
+        }
+
+        Tag otherRole = (Tag) other;
+        return role.equals(otherRole.tagName);
+    }
+
+    @Override
+    public int hashCode() {
+        return role.hashCode();
+    }
+
+    /**
+     * Format state as text for viewing.
+     */
+    public String toString() {
+        return '[' + role + ']';
+    }
+
+}
