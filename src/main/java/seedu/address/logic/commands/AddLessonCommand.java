66752f6f2d8658b775e97bdc496dd8606e75671d
jarvis <jarvischua8002@gmail.com>
2025-10-08 21:24:50 +0800

    Add AddLessonCommand and its parser classes

---
+++ src/main/java/seedu/address/logic/commands/AddLessonCommand.java
@@ -0,0 +1,77 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.lesson.Lesson;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+public class AddLessonCommand extends Command {
+    public static final String COMMAND_WORD = "addc";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a class to the address book. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_PHONE + "PHONE "
+            + PREFIX_EMAIL + "EMAIL "
+            + PREFIX_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_PHONE + "98765432 "
+            + PREFIX_EMAIL + "johnd@example.com "
+            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_TAG + "friends "
+            + PREFIX_TAG + "owesMoney";
+
+    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+
+    private final Lesson toAdd;
+
+    /**
+     * Creates an AddCommand to add the specified {@code Person}
+     */
+    public AddLessonCommand(Lesson lesson) {
+        requireNonNull(lesson);
+        toAdd = lesson;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasLesson(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        model.addLesson(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.formatLesson(toAdd)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddCommand)) {
+            return false;
+        }
+
+        AddLessonCommand otherAddLessonCommand = (AddLessonCommand) other;
+        return toAdd.equals(otherAddLessonCommand.toAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("toAdd", toAdd)
+                .toString();
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/AddLessonCommandParser.java
@@ -0,0 +1,52 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddLessonCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.lesson.*;
+import seedu.address.model.tag.Tag;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+public class AddLessonCommandParser implements Parser<AddLessonCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an AddCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected formatPerson
+     */
+    public AddLessonCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_CLASS, PREFIX_DAY,
+                        PREFIX_TIME, PREFIX_TUTOR);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_CLASS, PREFIX_DAY, PREFIX_TIME, PREFIX_TUTOR)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
+        ClassName className = ParserUtil.parseClassName(argMultimap.getValue(PREFIX_NAME).get());
+        Day day = ParserUtil.parseDay(argMultimap.getValue(PREFIX_DAY).get());
+        Time time = ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME).get());
+        Tutor tutor = ParserUtil.parseTutor(argMultimap.getValue(PREFIX_TUTOR).get());
+        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+        Lesson lesson = new Lesson(className, day, time, tutor, tagList);
+
+        return new AddLessonCommand(lesson);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+}
