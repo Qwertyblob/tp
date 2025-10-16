0557ca5ad78bbabde68a4cacfad685b09ca5905f
Isaac Lee <isaacleekyexen@gmail.com>
2025-10-16 16:42:31 +0800

    Add delete by name

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -18,12 +18,21 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
+<<<<<<< HEAD
         String trimmedArgs = args.trim();
 
         // Check if it's a name-based delete (starts with n/)
         if (trimmedArgs.startsWith("n/")) {
             try {
                 Name name = ParserUtil.parseName(trimmedArgs.substring(2));
+=======
+        String[] argsSplitNameFlag = args.split("n/");
+        String[] argsSplitSpace = args.split(" ");
+
+        if (argsSplitNameFlag.length == 2) {
+            try {
+                Name name = ParserUtil.parseName(argsSplitNameFlag[1]);
+>>>>>>> f2704fcc (Add delete by name)
                 return new DeleteCommand(name);
             } catch (ParseException pe) {
                 throw new ParseException(
@@ -31,10 +40,16 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             }
         }
 
+<<<<<<< HEAD
         // Check if it's an index-based delete (numeric)
         if (trimmedArgs.matches("^[0-9]+$")) {
             try {
                 Index index = ParserUtil.parseIndex(trimmedArgs);
+=======
+        if (argsSplitSpace.length == 2 && argsSplitSpace[1].matches("^[0-9]*[1-9][0-9]*$")) {
+            try {
+                Index index = ParserUtil.parseIndex(args);
+>>>>>>> f2704fcc (Add delete by name)
                 return new DeleteCommand(index);
             } catch (ParseException pe) {
                 throw new ParseException(
@@ -42,6 +57,10 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             }
         }
 
+<<<<<<< HEAD
         throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+=======
+        throw new ParseException(DeleteCommand.MESSAGE_USAGE);
+>>>>>>> f2704fcc (Add delete by name)
     }
 }
