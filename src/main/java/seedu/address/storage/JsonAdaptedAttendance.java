0a2be80d38abd6bf28588abb6dac1e8da0d523f9
dylan <mixedberries92@gmail.com>
2025-10-16 16:39:05 +0800

    Update lessons to save attendances

---
+++ src/main/java/seedu/address/storage/JsonAdaptedAttendance.java
@@ -0,0 +1,36 @@
+package seedu.address.storage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Jackson-friendly version of a daily attendance record.
+ */
+class JsonAdaptedAttendance {
+
+    private final String date;
+    private final List<JsonAdaptedIdentificationNumber> presentStudents = new ArrayList<>();
+
+    /**
+     * Constructs a {@code JsonAdaptedAttendance} with the given details.
+     */
+    @JsonCreator
+    public JsonAdaptedAttendance(@JsonProperty("date") String date,
+                                 @JsonProperty("presentStudents") List<JsonAdaptedIdentificationNumber> presentStudents) {
+        this.date = date;
+        if (presentStudents != null) {
+            this.presentStudents.addAll(presentStudents);
+        }
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public List<JsonAdaptedIdentificationNumber> getPresentStudents() {
+        return new ArrayList<>(presentStudents);
+    }
+}
