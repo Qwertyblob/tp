3213146452080e868d880ae2a252de1db2ca076f
yhtham7 <yhtham7@gmail.com>
2025-10-16 17:32:31 +0800

    Add EditLessonCommand and EditLessonCommandParser

---
+++ src/main/java/seedu/address/logic/parser/EditLessonCommandParser.java
@@ -0,0 +1,83 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLASS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DAY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTOR;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.EditLessonCommand;
+import seedu.address.logic.commands.EditLessonCommand.EditLessonDescriptor;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * Parses input arguments and creates a new EditLessonCommand object
+ */
+public class EditLessonCommandParser implements Parser<EditLessonCommand> {
+
+    @Override
+    public EditLessonCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_CLASS, PREFIX_DAY, PREFIX_TIME,
+                PREFIX_TUTOR, PREFIX_TAG);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditLessonCommand.MESSAGE_USAGE), pe);
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_CLASS, PREFIX_DAY, PREFIX_TIME, PREFIX_TUTOR);
+
+        EditLessonDescriptor descriptor = new EditLessonDescriptor();
+
+        if (argMultimap.getValue(PREFIX_CLASS).isPresent()) {
+            descriptor.setClassName(ParserUtil.parseClassName(argMultimap.getValue(PREFIX_CLASS).get()));
+        }
+
+        if (argMultimap.getValue(PREFIX_DAY).isPresent()) {
+            descriptor.setDay(ParserUtil.parseDay(argMultimap.getValue(PREFIX_DAY).get()));
+        }
+
+        if (argMultimap.getValue(PREFIX_TIME).isPresent()) {
+            descriptor.setTime(ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME).get()));
+        }
+        if (argMultimap.getValue(PREFIX_TUTOR).isPresent()) {
+            descriptor.setTutor(ParserUtil.parseTutor(argMultimap.getValue(PREFIX_TUTOR).get()));
+        }
+        parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(descriptor::setTags);
+
+
+        if (!descriptor.isAnyFieldEdited()) {
+            throw new ParseException(EditLessonCommand.MESSAGE_NOT_EDITED);
+        }
+
+        return new EditLessonCommand(index, descriptor);
+    }
+
+    /**
+     * Parses {@code Collection<String> tags} into a {@code Set<Tag>} if {@code tags} is non-empty.
+     * If {@code tags} contains only one element which is an empty string, it will be parsed into a
+     * {@code Set<Tag>} containing zero tags.
+     */
+    private Optional<Set<Tag>> parseTagsForEdit(Collection<String> tags) throws ParseException {
+        assert tags != null;
+
+        if (tags.isEmpty()) {
+            return Optional.empty();
+        }
+        Collection<String> tagSet = tags.size() == 1 && tags.contains("") ? Collections.emptySet() : tags;
+        return Optional.of(ParserUtil.parseTags(tagSet));
+    }
+}
