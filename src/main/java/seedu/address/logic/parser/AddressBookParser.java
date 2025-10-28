27269ede07854e82e2a2a7a04a70e5db63ab5ac0
jarvis <jarvischua8002@gmail.com>
2025-10-28 12:32:43 +0800

    Add undo feature in AddressBookParser

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -24,6 +24,7 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.ListLessonCommand;
 import seedu.address.logic.commands.MarkCommand;
+import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.commands.UnenrolCommand;
 import seedu.address.logic.commands.UnmarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -112,6 +113,9 @@ public class AddressBookParser {
 
         case UnenrolCommand.COMMAND_WORD:
             return new UnenrolCommandParser().parse(arguments);
+
+        case UndoCommand.COMMAND_WORD:
+            return new UndoCommand();
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
