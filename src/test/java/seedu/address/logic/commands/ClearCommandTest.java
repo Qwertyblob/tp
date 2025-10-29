77c81a412bee9bbb19af15180d601e44a5ddf674
Isaac Lee <isaacleekyexen@gmail.com>
2025-10-30 03:57:35 +0800

    Update tests

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -1,23 +1,33 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.assertConfirmableCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.assertConfirmationRequested;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.ConfirmationManager;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
 public class ClearCommandTest {
+    @Test
+    public void execute_nonEmptyAddressBook_force_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel.setAddressBook(new AddressBook());
+
+        assertCommandSuccess(new ClearCommand(true), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
 
     @Test
-    public void execute_emptyAddressBook_success() {
+    public void execute_nonEmptyAddressBook_requestsConfirmation() {
         Model model = new ModelManager();
-        Model expectedModel = new ModelManager();
-
-        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        ClearCommand clearCommand = new ClearCommand();
+        assertConfirmationRequested(clearCommand, model);
     }
 
     @Test
@@ -26,7 +36,9 @@ public class ClearCommandTest {
         Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel.setAddressBook(new AddressBook());
 
-        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        assertConfirmableCommandSuccess(new ClearCommand(), model,
+                new CommandResult(ClearCommand.MESSAGE_SUCCESS, CommandResult.DisplayType.DEFAULT),
+                expectedModel, new ConfirmationManager());
     }
 
 }
