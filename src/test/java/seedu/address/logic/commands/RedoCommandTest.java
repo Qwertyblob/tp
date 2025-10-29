10eab6d5d7f6c3a8efe7c2fd1ae0aa8c7249e6cd
jarvis <jarvischua8002@gmail.com>
2025-10-29 17:14:20 +0800

    Add RedoCommandTest

--- src/test/java/seedu/address/logic/commands/RedoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RedoCommandTest.java
@@ -1,4 +1,76 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for {@code RedoCommand}.
+ */
 public class RedoCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        // Set up a model with a VersionedAddressBook and some persons
+        model = new ModelManager();
+        model.addPerson(ALICE);
+        model.commitAddressBook();
+
+        model.addPerson(BENSON);
+        model.commitAddressBook();
+
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_singleRedo_success() {
+        // Undo first to create a redoable state
+        model.undoAddressBook();
+        assertEquals(1, model.getAddressBook().getPersonList().size());
+
+        // Expected model after redoing should match model before undo
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.addPerson(BENSON);
+        expectedModel.commitAddressBook();
+
+        assertCommandSuccess(new RedoCommand(), model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_multipleRedo_success() {
+        // Prepare a model with multiple states
+        model.undoAddressBook();
+        model.redoAddressBook(); // back to 2 persons
+        model.undoAddressBook(); // back to 1 person
+
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.addPerson(BENSON);
+        expectedModel.commitAddressBook();
+
+        assertCommandSuccess(new RedoCommand(), model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_noRedoableState_failure() {
+        // No redoable states initially
+        assertCommandFailure(new RedoCommand(), model, RedoCommand.MESSAGE_FAILURE);
+
+        // Undo once -> redo should now work once
+        model.undoAddressBook();
+        model.redoAddressBook();
+        assertCommandFailure(new RedoCommand(), model, RedoCommand.MESSAGE_FAILURE);
+    }
+
 }
