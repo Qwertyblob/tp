83c67642854e7e515cf24bff093b0c4a38dc987a
Isaac Lee <isaacleekyexen@gmail.com>
2025-10-30 04:07:07 +0800

    Rename test

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -15,7 +15,7 @@ import seedu.address.model.UserPrefs;
 
 public class ClearCommandTest {
     @Test
-    public void execute_nonEmptyAddressBook_force_success() {
+    public void execute_nonEmptyAddressBook_forceSuccess() {
         Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel.setAddressBook(new AddressBook());
