3118f2f133674488741a675c7743e8a9ee2ed666
jarvis <jarvischua8002@gmail.com>
2025-11-01 12:30:09 +0800

    Update program name in data and logs

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -37,7 +37,7 @@ import seedu.address.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 5, 0, true);
+    public static final Version VERSION = new Version(1, 6, 0, true);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
@@ -173,7 +173,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting Rollcall " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
